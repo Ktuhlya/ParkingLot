c6a3e036809b9f3afa4709576d07f0c0533b6e03
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-02-22 15:33:00 +0700

    123

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -3,9 +3,14 @@ package parking
 import kotlin.system.exitProcess
 
 var placeMap = mutableMapOf<Int, String>()
+var spot = 0
+var carList = mutableListOf<String>()
+
+const val EMPTY = "empty"
 
 fun main() {
-    for (i in 1..20) placeMap.set(i, "empty")
+
+        carList.add(0, EMPTY)
     choNado()
 
 }
@@ -14,23 +19,68 @@ fun choNado() {
   //  print("> ")
     val str = readln()
     val inputList = str.split(" ").toList()
+
     when(inputList[0]) {
+
         "leave" -> {
-            placeMap[inputList[1].toInt()] = "empty"
-            println("Spot ${inputList[1]} is free.")
-            choNado()
-        }
-        "park" -> {
-            if (!placeMap.containsValue("empty")) {
-                println("Sorry, the parking lot is full.")
+            if (placeMap.isEmpty()) {
+                println("Sorry, a parking lot has not been created.")
                 choNado()
-            }else{
-                var key = placeMap.keyOfValue("empty")
-                placeMap[key]  = inputList[2]
-              //  println(placeMap)
-                println("${inputList[2]} car parked in spot ${key}.")
+            } else {
+                placeMap[inputList[1].toInt()] = EMPTY
+                carList[inputList[1].toInt()] = EMPTY
+                println("Spot ${inputList[1]} is free.")
                 choNado()
             }
+        }
+        "park" -> {
+            if (placeMap.isEmpty()) {
+                println("Sorry, a parking lot has not been created.")
+                choNado()
+            } else {
+                if (!placeMap.containsValue(EMPTY)) {
+                    println("Sorry, the parking lot is full.")
+                    choNado()
+                } else {
+                    var key = placeMap.keyOfValue(EMPTY)
+                    placeMap[key] = inputList[2]
+                    carList[key] = "$key ${inputList[1]} ${inputList[2]}"
+                        //  println(placeMap)
+                    println("${inputList[2]} car parked in spot ${key}.")
+                    choNado()
+                }
+            }
+        }
+        "create" -> {
+            placeMap.clear()
+            carList.clear()
+            carList.add(0, EMPTY)
+            for (i in 1..inputList[1].toInt()) {
+                placeMap.set(i, EMPTY)
+                carList.add(i, EMPTY)
+            }
+
+            println("Created a parking lot with ${inputList[1]} spots.")
+            choNado()
+        }
+        "status" -> {
+            if (placeMap.isEmpty()) {
+                println("Sorry, a parking lot has not been created.")
+                choNado()
+            }else{
+            if (carList.count{it== EMPTY} < carList.size ) {
+
+                for (i in 1 until carList.size) {
+                    if (carList[i] != EMPTY) {
+                        println(carList[i])
+                    }
+                }
+                choNado()
+            } else{
+                println("Parking lot is empty.")
+                choNado()
+            }
+        }
         }
         "exit" -> exitProcess(0)
     }
